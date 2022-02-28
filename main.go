8e7ea374b875b66e8a0a50d108d49e174c5085e3
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-02-28 22:16:56 +0100

    html generator (#2)

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"text/template"
+)
+
+type Project struct {
+	Icon    string
+	URL     string
+	Title   string
+	Content string
+}
+
+type Me struct {
+	Projects []Project
+}
+
+func (m *Me) rebuildHTML() {
+	t, err := template.New("html").ParseFiles("./templates/index.html.tpl")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.OpenFile("index.html", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if err = t.Lookup("index.html.tpl").Execute(f, m); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	meJSON, err := ioutil.ReadFile("./templates/me.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var me Me
+	if err = json.Unmarshal(meJSON, &me); err != nil {
+		log.Fatal(err)
+	}
+
+	me.rebuildHTML()
+}
